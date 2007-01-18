daf6946b86351dbd970dd59ccecfd455cc0d175e
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-01-18 14:06:17 +0000

    Add "home" link to footer until proper navigation complete

--- www/_footer.php
+++ www/_footer.php
@@ -2,8 +2,11 @@
 <p class="meta">
 <a id="valid" href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="[XHTML 1.0 Valid]" /></a>
 
+<a href="<?= $script_root ?>/index.php">Home</a>
+ |
 <a href="https://garage.maemo.org/projects/mud-builder/">Garage project page</a>
- | &copy; Andrew Flegg 2007. Released under the Artistic Licence.
+ |
+&copy; Andrew Flegg 2007. Released under the Artistic Licence.
 </p>
 </body>
 </html>
