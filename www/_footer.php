450c8841d0e90109dbb588503895cc529559d555
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-01-18 13:04:52 +0000

    Extract standard header/footer for website

---
+++ www/_footer.php
@@ -0,0 +1,10 @@
+
+<p class="meta">
+<a id="valid" href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="[XHTML 1.0 Valid]" /></a>
+
+<a href="https://garage.maemo.org/projects/mud-builder/">Garage project page</a>
+ | &copy; Andrew Flegg 2007. Released under the Artistic Licence.
+</p>
+</body>
+</html>
+
---
+++ www/_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/x
+html1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+  <title>mud-builder<? if ($title != '') echo ": $title"?></title>
+  <link rel="stylesheet" href="/style.css" type="text/css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>mud-builder</h1>
+<? if ($title != '') echo "<h2>$title</h2>" ?>
+
