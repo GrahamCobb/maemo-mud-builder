5b505fb54e4f0b1cccf7c82dce22f858c8e2a082
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-01-18 13:29:26 +0000

    Improve styling of documentation pages

--- www/docs/index.php
+++ www/docs/index.php
@@ -1,13 +1,25 @@
 <? $title = 'Documentation';
    include("../_header.php") ?>
 
-<ol id="index">
 <?php
+
+$id    = preg_replace('/[^a-z0-9]+/i', '', $_GET[id]);
+$curId = '';
+$head  = '';
+?>
+
+<?php
+
+echo "<ol id=\"index\"";
+if ($id != '') echo " class=\"nav\"";
+echo ">\n";
 
 if (!($fp = @fopen("./structure.xml", "r"))) die("Couldn't open XML.");
 if (!($xml_parser = xml_parser_create())) die("Couldn't create parser.");
 
 function startElement($parser, $name, $attrib) {
+    global $id;
+    global $curId;
     switch ($name) {
         case $name == "SECTION" : {
             echo "<li><span class=\"section\">$attrib[TITLE]</span><ol>\n";
@@ -15,7 +27,10 @@ function startElement($parser, $name, $attrib) {
         }
 
         case $name == "DOC" : {
-            echo "<li><a href=\"?id=$attrib[FILE]\">";
+            echo "<li";
+            $curId = $attrib[FILE];
+            if ($curId == $id) echo " class=\"selected\"";
+            echo "><a href=\"index.php?id=$curId\">";
             break;
         }
     }
@@ -36,25 +51,27 @@ function endElement($parser, $name) {
 }
 
 function characterData($parser, $data) {
+    global $id, $curId, $head;
     echo $data;
+
+    if ($id == $curId) $head .= $data;
 }
 
 xml_set_element_handler($xml_parser, "startElement", "endElement");
 xml_set_character_data_handler($xml_parser, "characterData");
 
-while( $data = fread($fp, 4096)){
-if(!xml_parse($xml_parser, $data, feof($fp))) {
-break;}}
+while( $data = fread($fp, 4096)) {
+    if(!xml_parse($xml_parser, $data, feof($fp))) reak;
+}
 xml_parser_free($xml_parser); 
 ?>
 </ol>
 
-<?
-if ($_GET[id] != '') {
-    $id = preg_replace('/[^a-z0-9]+/i', '', $_GET[id]);
+<? 
+if ($id != '') {
+    echo "<h2 id=\"$id\">$head</h2>";
     include("./$id.html");
 }
 ?>
-    
 
 <? include("../_footer.php") ?>
