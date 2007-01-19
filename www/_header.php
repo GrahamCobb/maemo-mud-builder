70768d61a82f452dcb429be272ac720e1e1b0b81
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-01-19 15:26:21 +0000

    Fix doctype (reported by Niels Breet)

--- www/_header.php
+++ www/_header.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/x
-html1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?
   $script_root = $_SERVER['PHP_SELF'];
   $script_root = dirname($script_root);
