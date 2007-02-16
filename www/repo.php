fd09bec43fff862c482477802f46f2260543ccba
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-02-16 14:30:23 +0000

    Can't use 302 redirects for Debian repos. Use mod_rewrite on bleb.org instead.

--- www/repo.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $base = 'http://bleb.org/software/maemo/repo/';
-  $file = $_SERVER["REQUEST_URI"];
-  $file = preg_replace('/.*\//', '', $file);
-  header("Location: $base$file");
-
-  exit;
-?>
