01649bc159297ec01bbb1bf2364fe09e8940445f
jaffa <jaffa@6cd10845-f819-0410-b525-fc0c5e1c5c8b>
2007-11-21 21:03:59 +0000

    Try fixing website a bit more

--- www-src/_header.php
+++ www-src/_header.php
@@ -1,17 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<?
-  $script_root = $_SERVER['PHP_SELF'];
-  $script_root = dirname($script_root);
-
-  for ($i = 0; $i < 5 && $script_root != '/' && !is_file("$_SERVER[DOCUMENT_ROOT]/$script_root/style.css"); $i++) {
-      $script_root = dirname($script_root);
-   }
-   if ($script_root == '/') $script_root = '';
-?>
 <html>
 <head>
   <title>mud-builder<? if ($title != '') echo ": $title"?></title>
-  <link rel="stylesheet" href="<?= $script_root ?>/style.css" type="text/css" />
+  <link rel="stylesheet" href="/style.css" type="text/css" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
